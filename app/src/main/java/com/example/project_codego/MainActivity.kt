f98d72a1eb865a7f493e2eac676061fd7f5d28ed
ynion-mabeamae <mabeamaeynion@gmail.com>
2026-01-14 08:42:42 +0800

    Add import LocalContext

--- app/src/main/java/com/example/project_codego/MainActivity.kt
+++ app/src/main/java/com/example/project_codego/MainActivity.kt
@@ -41,6 +41,7 @@ import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
